000db94b14c21806f4e18869f3670c414792030f
sejalbahekar123 <sejalbahekar123@gmail.com>
2026-02-12 17:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-class Rectangle {
-	constructor(width,height){
-		this._width=width;
-		this._height=height;
-	}
-	get width()
-	{
-		return this._width;
-	}
-	get height()
-	{
-		return this._height;
-	}
-	getArea()
-	{
-		return this._width*this._height;
-	}
-}
-
-class Square extends Rectangle {
-	constructor(side)
-	{
-		super(side,side);
-	}
-	getPerimeter()
-	{
-		return 4 * this._width;
-	}
-	
-}
-//complete this code
 // class Rectangle {
-// Constructor(width,height){ if (width <= 0 || height <= 0) {
-//       throw new Error("Width and Height must be positive integers");
-//     }
-// 	this._width = width;
-// 	this._height = height;
+// 	constructor(width,height){
+// 		this._width=width;
+// 		this._height=height;
+// 	}
+// 	get width()
+// 	{
+// 		return this._width;
+// 	}
+// 	get height()
+// 	{
+// 		return this._height;
+// 	}
+// 	getArea()
+// 	{
+// 		return this._width*this._height;
+// 	}
 // }
-//  get width(){
-// return this._width;
-	
-// }
-
-// get height(){
-// 	return this._height;
-// }
-// getArea(){
-// 	return this._width * this._height;
-// }}
 
 // class Square extends Rectangle {
-// Constructor(side){
-// if (side <= 0) {
-//       throw new Error("Side must be a positive integer");
-//     }
-//   // Call parent constructor
-//     super(side, side);
+// 	constructor(side)
+// 	{
+// 		super(side,side);
+// 	}
+// 	getPerimeter()
+// 	{
+// 		return 4 * this._width;
+// 	}
+	
 // }
+// complete this code
+class Rectangle {
+constructor(width,height){
+	this._width = width;
+	this._height = height;
+}
+ get width(){
+return this._width;
+	
+}
 
-// getPerimeter(){
-//   return this._width * 4;
-// }
+get height(){
+	return this._height;
+}
+getArea(){
+	return this._width * this._height;
+}}
+
+class Square extends Rectangle {
+Constructor(side){
+
+  // Call parent constructor
+    super(side, side);
+}
+
+getPerimeter(){
+  return this._width * 4;
+}
 
 // }
 // const rectangle = new Rectangle(5, 10);
