279cb6e5fc76b8603442879f1532da28262c4d0c
sejalbahekar123 <sejalbahekar123@gmail.com>
2026-02-12 17:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
-//complete this code
 class Rectangle {
-Constructor(width,height){ if (width <= 0 || height <= 0) {
-      throw new Error("Width and Height must be positive integers");
-    }
-	this._width = width;
-	this._height = height;
+	constructor(width,height){
+		this._width=width;
+		this._height=height;
+	}
+	get width()
+	{
+		return this._width;
+	}
+	get height()
+	{
+		return this._height;
+	}
+	getArea()
+	{
+		return this._width*this._height;
+	}
 }
- get width(){
-return this._width;
-	
-}
-
-get height(){
-	return this._height;
-}
-getArea(){
-	return this._width * this._height;
-}}
 
 class Square extends Rectangle {
-Constructor(side){
-if (side <= 0) {
-      throw new Error("Side must be a positive integer");
-    }
-  // Call parent constructor
-    super(side, side);
+	constructor(side)
+	{
+		super(side,side);
+	}
+	getPerimeter()
+	{
+		return 4 * this._width;
+	}
+	
 }
+//complete this code
+// class Rectangle {
+// Constructor(width,height){ if (width <= 0 || height <= 0) {
+//       throw new Error("Width and Height must be positive integers");
+//     }
+// 	this._width = width;
+// 	this._height = height;
+// }
+//  get width(){
+// return this._width;
+	
+// }
 
-getPerimeter(){
-  return this._width * 4;
-}
+// get height(){
+// 	return this._height;
+// }
+// getArea(){
+// 	return this._width * this._height;
+// }}
 
-}
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.width);      // 5
-console.log(rectangle.height);     // 10
-console.log(rectangle.getArea());  // 50
+// class Square extends Rectangle {
+// Constructor(side){
+// if (side <= 0) {
+//       throw new Error("Side must be a positive integer");
+//     }
+//   // Call parent constructor
+//     super(side, side);
+// }
 
-const square = new Square(7);
-console.log(square.width);         // 7
-console.log(square.height);        // 7
-console.log(square.getArea());     // 49
-console.log(square.getPerimeter()); // 28
-// const rectangle = new Rectangle(10,15);
-// const square = new Square(4);
-// Square.getArea();
-// Square.getPerimeter();
+// getPerimeter(){
+//   return this._width * 4;
+// }
 
+// }
+// const rectangle = new Rectangle(5, 10);
+// console.log(rectangle.width);      // 5
+// console.log(rectangle.height);     // 10
+// console.log(rectangle.getArea());  // 50
+
+// const square = new Square(7);
+// console.log(square.width);         // 7
+// console.log(square.height);        // 7
+// console.log(square.getArea());     // 49
+// console.log(square.getPerimeter()); // 28
 
 
 
